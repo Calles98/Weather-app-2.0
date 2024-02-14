fd487425f5f450a7d0fe765e3c9aa0506820479c
Rodrigo Calles <joserodrigoc98@gmail.com>
2024-02-14 09:58:07 -0700

    Adding home page

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-700 to-black p-10 flex flex-col justify-center items-center">
+      <Card className="max-w-4xl mx-auto bg-white rounded-xl">
+        <Text className="text-6xl font-bold text-center mb-10 opacity-50">
+          Weather AI
+        </Text>
+        <Subtitle className="text-xl text-center opacity-35">
+          Powed by OpenAI, Next.js 14, Tailwind CSS, Tremor and more!
+        </Subtitle>
+
+        <Divider className="my-10 border" />
+
+        <Card className="bg-gradient-to-br from-blue-700 to-black p-10 rounded-xl">
+          {/* CityPicker */}
+        </Card>
+      </Card>
+    </div>
+  );
+}
